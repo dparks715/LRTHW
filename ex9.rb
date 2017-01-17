cc286bfb2ba0d82061442dff237c60fa8d4d5fdf
Daniel Parks <dparks715@yahoo.com>
2017-01-17 03:27:39 -0500

    changed {} delimter to !

--- ex9.rb
+++ ex9.rb
@@ -1,14 +1,15 @@
 # assigning variables, \n creates a new line
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "\njan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
-# %q acts like quotes
-puts %q{
+# %q instead of quotes so apostrophes in the string dont get confused
+# used ! as a delimter instead of {}
+puts %q!
 	There's something going on here.
 	With the three double-quotes.
 	We'll be able to type as much as we like.
 	Even 4 lines if we want, or 5, or 6.
-}
+!
