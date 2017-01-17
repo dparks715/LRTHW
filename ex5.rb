1dec3bf64fb2096f1b1f974f45d5ad1a135cd7f5
Daniel Parks <dparks715@yahoo.com>
2017-01-17 01:45:59 -0500

    first commit

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+# Set variables
+my_name = 'Dan Parks'
+my_age = 30
+my_height = 64 # inches
+my_weight = 175 # pounds
+my_eyes = 'green'
+my_teeth = 'white'
+my_hair = 'brown'
+
+# Printed text with inserted variables.
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# Did some math inside a string using variables instead of numbers.
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
