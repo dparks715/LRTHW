bcb6464fe84daf8da246996ffb67c279997fe3f4
Daniel Parks <dparks715@yahoo.com>
2017-01-17 01:19:39 -0500

    added comments

--- ex3.rb
+++ ex3.rb
@@ -1,16 +1,22 @@
+# Prints statement
 puts "I will now count my chickens:"
 
+# Prints text, math inserted into string via #{}
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
 puts "Now I will count the eggs:"
 
+# Prints result of formula
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Prints numbers instead of doing math because inside string
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# Returns false 5 is greater -2
 puts 3 + 2 < 5 - 7
 
+# Performs calculations inside string using #{}
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
 
@@ -18,6 +24,7 @@ puts "Oh, that's why it's false."
 
 puts "How about some more."
 
+# Returns true or false based on values in #{}
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
