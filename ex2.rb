ba2c2174dcaa3f8634f0fc80affea7437d4a8418
Daniel Parks <dparks715@yahoo.com>
2017-01-17 01:06:11 -0500

    # inside string

--- ex2.rb
+++ ex2.rb
@@ -7,3 +7,6 @@ puts "I could have code like this." # and the comment after is ignored
 # puts "This won't run."
 
 puts "This will run."
+
+# Octothorpe/pound sign in string prints normally
+puts "Hi # there"
