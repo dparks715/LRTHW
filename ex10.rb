772c7390a336a93457b052ca1b9838949f946b20
Daniel Parks <dparks715@yahoo.com>
2017-01-17 05:34:34 -0500

    changed back to double quotes

--- ex10.rb
+++ ex10.rb
@@ -3,12 +3,12 @@ persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
 # changed to single quotes, escape sequences just get printed
-fat_cat = '''
-I\'ll do a list:
+fat_cat = """
+I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-'''
+"""
 
 puts tabby_cat
 puts persian_cat
