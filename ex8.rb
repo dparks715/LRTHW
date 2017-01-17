70d75c8f551e524bd18c5a23e7897eb4cf9721a1
Daniel Parks <dparks715@yahoo.com>
2017-01-17 03:14:53 -0500

    insert values into the format within format

--- ex8.rb
+++ ex8.rb
@@ -7,7 +7,13 @@ puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
 puts formatter % {first: true, second: false, third: true, fourth: false}
 
 # putting the entire format in each section of original format
-puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+# passing values to the "inner" variables
+puts formatter % {
+	first: formatter % {first: 1, second: 2, third: 3, fourth: 4},
+	second: formatter % {first: "one", second: "two", third: "three", fourth: "four"},
+	third: formatter % {first: true, second: false, third: true, fourth: false},
+	fourth: formatter % {first: "mined", second: "minds", third: "mined minds", fourth: "hello"}
+}
 
 # same as above, but cleaner
 puts formatter % {
