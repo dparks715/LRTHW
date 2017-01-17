7a6f7034710f3364dc0948709e4628d1c9f021ff
Daniel Parks <dparks715@yahoo.com>
2017-01-17 06:01:35 -0500

    added 4th variable

--- ex13.rb
+++ ex13.rb
@@ -1,5 +1,6 @@
-first, second, third = ARGV
+first, second, third, fourth = ARGV
 
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+puts "Your fourth variable is: #{fourth}"
