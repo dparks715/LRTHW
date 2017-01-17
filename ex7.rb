e8a7e3857cb6ea5557d634880c39e8d0ee73160d
Daniel Parks <dparks715@yahoo.com>
2017-01-17 02:42:41 -0500

    first

---
+++ ex7.rb
@@ -0,0 +1,22 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}." # snow not a variable
+puts "And everywhere that Mary went."
+puts "." * 10  # prints the string . 10 times
+
+# assign variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print doesn't start a new line, so lines 21 and 22 print on the same line
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
