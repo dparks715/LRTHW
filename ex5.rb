4f7b9676ef51de90f8e607c0e0f97ec9d1d40d01
Daniel Parks <dparks715@yahoo.com>
2017-01-17 01:49:42 -0500

    added variables for cen, kilo conversion ratios

--- ex5.rb
+++ ex5.rb
@@ -6,11 +6,13 @@ weight = 175 # pounds
 eyes = 'green'
 teeth = 'white'
 hair = 'brown'
+cen = 2.54
+kilo = 0.45
 
-# Printed text with inserted variables.
+# Printed text with inserted variables.  Used conversion ratios for centimeters and kilograms.
 puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{height} inches tall.  That's #{height * cen} in centimeters."
+puts "He's #{weight} pounds heavy.  That's #{weight * kilo} in kilograms"
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
