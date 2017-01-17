f49ce07535deeb746bbad5abb8e16634a5547df6
Daniel Parks <dparks715@yahoo.com>
2017-01-17 05:47:29 -0500

    used gets.chomp.to_i, called class to see changes

--- ex11.rb
+++ ex11.rb
@@ -1,11 +1,14 @@
 print "How old are you? "
-age = gets.chomp
+age = gets.chomp  # gets user input as string
 print "How tall are you? "
-height = gets.chomp
+height = gets.chomp.to_i  # gets user input as fixnum
 print "How much do you weigh? "
-weight = gets.chomp
+weight = gets.chomp.to_i
 
 puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+# displays the class of our user inputs, string and fixnum, so we know what to_i does
+puts "gets.chomp gives us a #{age.class}, while gets.chomp.to_i gives us #{height.class}."
 
 # testing chomp
 # chomp removes the \n from x
