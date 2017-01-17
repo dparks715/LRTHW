2dd5ffa1f67c2e5e93e3f3fa5e4af5b9d04a7453
Daniel Parks <dparks715@yahoo.com>
2017-01-17 01:48:11 -0500

    shortened variable names

--- ex5.rb
+++ ex5.rb
@@ -1,19 +1,19 @@
 # Set variables
-my_name = 'Dan Parks'
-my_age = 30
-my_height = 64 # inches
-my_weight = 175 # pounds
-my_eyes = 'green'
-my_teeth = 'white'
-my_hair = 'brown'
+name = 'Dan Parks'  # Don't know why name changed color, still works.
+age = 30
+height = 64 # inches
+weight = 175 # pounds
+eyes = 'green'
+teeth = 'white'
+hair = 'brown'
 
 # Printed text with inserted variables.
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # Did some math inside a string using variables instead of numbers.
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
