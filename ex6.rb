dd8a9566a5350f36500ffe1ba7d4fc6924d74128
Daniel Parks <dparks715@yahoo.com>
2017-01-17 02:16:46 -0500

    undid last commit, script works again

--- ex6.rb
+++ ex6.rb
@@ -1,17 +1,16 @@
-# USING SINGLE QUOTES - appears to not call variables using #{}
 # Assigning strings to variables, and calling variables within these.
 types_of_people = 10
-x = 'There are #{types_of_people} types of people.'
-binary = 'binary'
-do_not = "don't" # single quotes here get confused due to the quote in "don't"
-y = 'Those who know #{binary} and those who #{do_not}.'
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
 # Prints x and y which are strings
 puts x
 puts y
 
 # Calls variables inside printed strings
-puts 'I said: #{x}.'
-puts "I also said: '#{y}'." # single quotes confused again
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
 
 # assign variables
 hilarious = false
