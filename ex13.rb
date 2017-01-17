da467f20d948018036e26cf698e08096443938c6
Daniel Parks <dparks715@yahoo.com>
2017-01-17 06:02:13 -0500

    removed 2 variables

--- ex13.rb
+++ ex13.rb
@@ -1,6 +1,4 @@
-first, second, third, fourth = ARGV
+first, second = ARGV
 
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
-puts "Your third variable is: #{third}"
-puts "Your fourth variable is: #{fourth}"
