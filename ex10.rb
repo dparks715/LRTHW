4b0b41aa616626b600fe60e177020f7d6f680a39
Daniel Parks <dparks715@yahoo.com>
2017-01-17 05:30:00 -0500

    changed to single quotes

--- ex10.rb
+++ ex10.rb
@@ -2,12 +2,13 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
-I'll do a list:
+# changed to single quotes, escape sequences just get printed
+fat_cat = '''
+I\'ll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 puts tabby_cat
 puts persian_cat
